e56eea4d103f50d52329131eb7f6a674132ff6d2
Joao Jesus <johncjesus@gmail.com>
2023-05-05 10:00:06 +0100

    log

--- src/helpers/log.ts
+++ src/helpers/log.ts
@@ -1,5 +1,17 @@
+function getTrace() {
+    const error = new Error();
+    const stackLines = error.stack?.split('\n');
+    const callerInfo = stackLines ? stackLines[2] : ''; // The 3rd line in the stack trace contains the caller's info
+
+    const fileInfoRegex = /\((.+)\)/; // Regex to extract the file path, line number, and column number
+    const fileInfoMatch = fileInfoRegex.exec(callerInfo);
+    const fileInfo = fileInfoMatch ? fileInfoMatch[1] : 'unknown';
+
+    console.log('Called from:', fileInfo);
+}
+
 export const log = (...content: any) => {
-    if(process.env.REACT_APP_DEBUG?.toLowerCase() === 'true') {
-        console.log(...content);
+    if (process.env.REACT_APP_DEBUG?.toLowerCase() === 'true') {
+        console.log(getTrace(), ...content);
     }
 }
